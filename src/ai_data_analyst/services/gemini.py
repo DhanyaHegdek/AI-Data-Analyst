a1f937a3316bd800f95bd2916fb6bde00db8e405
dhanyashreemh <dhanyashree@raakacloud.com>
2026-09-11 17:24:07 +0530

    added gemini

---
+++ src/ai_data_analyst/services/gemini.py
@@ -0,0 +1,10 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from ai_data_analyst.core.config import GEMINI_API_KEY
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash",
+    google_api_key=GEMINI_API_KEY,
+    temperature=0,
+)
